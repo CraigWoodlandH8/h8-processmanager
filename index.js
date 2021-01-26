ece881416e2164c5d1b0e7d2c8efbf07b25a4c00
Craig Woodland <craig.woodland@herotech8.com>
2021-01-26 17:56:03 +0000

    Add option to disable verbose output

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 const { spawn } = require("child_process");
 
 class Process {
-  constructor(name, basedir) {
+  constructor(name, basedir, verboseOutput) {
     this.filename = 'modules/' + name + '.js';
 
     if(basedir !== undefined) {
       this.filename = basedir + '/' + this.filename;
     }
 
+    if(verboseOutput !== undefined && verboseOutput) {
+      this.verboseOutput = true;
+    } else {
+      this.verboseOutput = false;
+    }
+
     this.run();
   }
 
   run() {
-    console.log('[Process]', '[Run]', this.filename);
+    if(this.verboseOutput) {
+      console.log('[Process]', '[Run]', this.filename);
+    }
 
     var parent = this, child = spawn('node', [ this.filename ]);
 
     child.stdout.on('data', (data) => {
-      console.log('[Process]', '[stdout]', parent.filename, data);
+      if(parent.verboseOutput) {
+        console.log('[Process]', '[stdout]', parent.filename, data);
+      }
     });
 
     child.stderr.on('data', (data) => {
-      console.log('[Process]', '[stderr]', parent.filename, data);
+      if(parent.verboseOutput) {
+        console.log('[Process]', '[stderr]', parent.filename, data);
+      }
     });
 
     child.on('error', (err) => {
-      console.log('[Process]', '[Error]', parent.filename, err);
+      if(parent.verboseOutput) {
+        console.log('[Process]', '[Error]', parent.filename, err);
+      }
     });
 
     child.on('exit', (code) => {
-      console.log('[Process]', '[Exit]', parent.filename, code);
+      if(parent.verboseOutput) {
+        console.log('[Process]', '[Exit]', parent.filename, code);
+      }
 
       setTimeout(() => {
         parent.run();
